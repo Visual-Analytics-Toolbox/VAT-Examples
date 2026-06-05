cebaf1b71dc1dcad25c37dcdcbba2f12d36f1e1c
emma schlotter <57963283+emmaSC2@users.noreply.github.com>
2026-06-05 20:31:02 +0200

    chore: improve examples

--- models/event_examples.py
+++ models/event_examples.py
@@ -1,12 +1,26 @@
 from vaapi.client import Vaapi
 import os
-
+from datetime import datetime,timedelta
 
 def get_events():
     response = client.events.list()
     for event in response:
         print(event)
 
+def get_event(id):
+    response = client.events.get(id=id)
+    print(response)
+
+def create_event():
+    response = client.events.create(name="GO_27",is_testevent=False,start_day=datetime.today().strftime('%Y-%m-%d'),end_day=(datetime.today() + timedelta(days=4)).strftime('%Y-%m-%d'))
+    return response
+
+def update_event(id):
+    response = client.events.update(id=id,comment="everything was fine :)")
+    print(response)
+
+def delete_event(id):
+    client.events.delete(id=id)
 
 if __name__ == "__main__":
     client = Vaapi(
@@ -15,3 +29,8 @@ if __name__ == "__main__":
     )
 
     get_events()
+    event = create_event()
+    get_event(event.id)
+    update_event(event.id)
+    delete_event(event.id)
+    
--- models/game_examples.py
+++ models/game_examples.py
@@ -1,11 +1,33 @@
 from vaapi.client import Vaapi
 import os
-
+from datetime import datetime
 
 def get_games():
     response = client.games.list()
     for game in response:
         print(game)
+
+def get_game(id):
+    response = client.games.get(id=id)
+    print(response)
+
+# game_folder="None" filters for games where this field is null
+def filter_games(event=None,is_testgame=None):
+    response = client.games.list(event=event,is_testgame=is_testgame,game_folder="None",half="half1")
+    for game in response:
+        print(game)
+
+def create_game():
+    response = client.games.create(event=10,team1=4,team2=5,half="half1",start_time=datetime.today().strftime('%Y-%m-%d_%h-%m-%s'))
+    return response
+
+def update_game(id):
+    response = client.games.update(id=id,comment="everything was fine :)")
+    print(response)
+
+def delete_game(id):
+    client.game.delete(id=id)
+
 
 if __name__ == "__main__":
     client = Vaapi(
@@ -14,3 +36,8 @@ if __name__ == "__main__":
     )
 
     get_games()
+    filter_games(10,False)
+    game = create_game()
+    get_game(game.id)
+    update_game(game.id)
+    delete_game(game.id)
